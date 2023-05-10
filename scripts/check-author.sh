4f6872e957e7ecfab263cfd0756b4e4124c47fba
Alejandro R Mosteo <amosteo@unizar.es>
2023-05-10 12:23:51 +0200

    Add script to check maintainership (#16)

---
+++ scripts/check-author.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
+trap 'echo "Interrupted" >&2 ; exit 1' INT
+
+set -o errexit
+set -o nounset
+
+# Import reusable bits
+pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. common.sh
+popd
+
+# Required for aliases to work in non-interactive scripts
+shopt -s expand_aliases
+
+# Ensure all alr runs are non-interactive and able to output unexpected errors
+alias alr="alr -d -n"
+
+# See whats happening
+git log --graph --decorate --pretty=oneline --abbrev-commit --all | head -30
+
+# Get author and show for the record
+PR_AUTHOR=$1
+echo PR author: $PR_AUTHOR
+
+# Detect changes
+CHANGES=$( changed_manifests )
+
+# Bulk changes for the record
+echo Changed files: $CHANGES
+
+# Enable Homebrew on macOS
+[ `uname -s` == "Darwin" ] && {
+   eval $(brew shellenv)
+   echo "Homebrew for macOS enabled"
+}
+
+# Configure local index
+alr index --name local --add ./index
+
+# Remove community index in case it has been added somehow
+alr index --del community || true
+
+# Match maintainers against PR author
+for file in $CHANGES; do
+
+   if [[ $file == index.toml ]]; then
+      echo Skipping index metadata file: $file
+      continue
+   fi
+
+   if [[ $file != *.toml ]]; then
+      echo Skipping non-crate file: $file
+      continue
+   fi
+
+   if ! [ -f ./$file ]; then
+      echo Skipping deleted file: $file
+      continue
+   fi
+
+   # Checks passed, this is a crate we must test
+   is_system=false
+
+   crate=$(basename $file .toml | cut -f1 -d-)
+   version=$(basename $file .toml | cut -f2- -d-)
+   milestone="$crate=$version"
+   previous="$crate<$version"
+
+   echo
+   box "$milestone"
+   echo
+
+   # Remember that version can be "external", in which case we do not have yet
+   # a reliable way of checking maintainership, as properties can't be listed
+   # right now for undetected externals.
+
+   if [[ $version = external ]]; then
+      echo SKIPPING check for external crate $milestone
+      continue
+   fi
+
+   # If the file already existed, we can't be sure that the maintainers aren't
+   # being modified without going into previous index commits. For now, we
+   # simply fail as it is not usual to modify existing manifests. This ensures
+   # than when adding a new crate, all previous ones aren't modified to change
+   # maintainers there.
+   if exists_in_base $file; then
+      fail "FAILED: modified $file already existed; ensure manually no maintainers were modified"
+   fi
+
+   # If there is no previous version, there's no test to perform
+   if alr show $previous | grep -q "Not found"; then
+      echo SKIPPING check for first release $milestone
+      continue
+   fi
+
+   # Show all maintainers for the record (using previous release)
+   echo $previous maintainers are:
+   alr show $previous | head -1
+   alr show $previous | grep Maintainers_Logins
+
+   for maint in $(alr show $previous | grep Maintainers_Logins: | cut -f2 -d: | tr -d ' '); do
+      if [[ $maint = $PR_AUTHOR ]]; then
+         echo SUCCESS: PR author $PR_AUTHOR is in the list of maintainers
+         continue 2 # Skip to next milestone
+      else
+         echo Maintainer $maint does not match PR author $PR_AUTHOR
+      fi
+   done
+
+   fail FAILED: PR author is not in the list of maintainers of $previous
+
+done
+
+exit 0
--- scripts/common.sh
+++ scripts/common.sh
@@ -17,3 +17,23 @@ function changed_manifests() {
         git diff --name-only HEAD~1
     fi
 }
+
+function fail() {
+    # Fail if not DRY_RUN is set, otherwise just print the $1 error
+    if [[ "${DRY_RUN:-unset}" == "unset" ]]; then
+        echo $@
+        exit 1
+    else
+        echo $@
+        echo "DRY_RUN set, continuing"
+    fi
+}
+
+function exists_in_base() {
+    # Check whether $1 exists in the base commit
+    if [[ -n $(git ls-tree HEAD~1 --name-only "$1") ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
